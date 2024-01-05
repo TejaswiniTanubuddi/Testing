c5943490b51b9b7593e08fc79f21509075d8b72b
TejaswiniTanubuddi <tejaswini.tanubuddi@gmail.com>
2024-01-05 11:23:11 -0700

    modified

--- src/test/java/com/wfh/launch.java
+++ src/test/java/com/wfh/launch.java
@@ -1,12 +1,12 @@
 package com.wfh;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class launch {
 
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
 		driver.close();
